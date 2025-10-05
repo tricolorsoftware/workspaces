6f16caddc00de791b36b63814c69d94238e9c899
ewsgit <ewsgit@ewsgit.uk>
2025-10-05 23:31:29 +0000

    UserSelect improvements

--- packages/web-frontend/src/pages/userSelect/UserSelect.tsx
+++ packages/web-frontend/src/pages/userSelect/UserSelect.tsx
@@ -1,7 +1,6 @@
-import type { Component } from "solid-js";
+import  { type Component, createSignal} from "solid-js";
 import UKCard from "@tricolor/uikit-solid/src/components/card/UKCard.tsx";
 import UKButton from "@tricolor/uikit-solid/src/components/button/UKButton.tsx";
-import UKButtonGroup from "@tricolor/uikit-solid/src/components/buttonGroup/UKButtonGroup.tsx";
 import UKDivider from "@tricolor/uikit-solid/src/components/divider/UKDivider.tsx";
 import { DividerDirection } from "@tricolor/uikit-solid/src/components/divider/lib/direction.ts";
 import UKText from "@tricolor/uikit-solid/src/components/text/UKText.tsx";
@@ -9,29 +8,47 @@ import styles from "./UserSelect.module.scss"
 import UKTextField from "@tricolor/uikit-solid/src/components/textField/UKTextField.tsx";
 
 const UserSelectPage: Component = () => {
+    const [username, setUsername] = createSignal("");
+    const [password, setPassword] = createSignal("");
+
     return (
-            <UKCard color={"filled"} class={styles.root}>
-                <UKText role={"title"} size={"l"}>
+        <div class={styles.root}>
+            <UKCard color={"filled"} class={styles.modal}>
+                <UKText role={"title"} size={"l"} emphasized={true}>
                     Sign In
                 </UKText>
                 <UKDivider direction={DividerDirection.horizontal} />
-                <UKTextField color={"outlined"} label={"Username"}/>
-                <UKTextField color={"outlined"} label={"Password"}/>
-                <UKButtonGroup size={"s"}>
-                    <UKButton onClick={() => 0} color={"standard"}>
+                <UKTextField color={"outlined"} label={"Username"} getValue={setUsername} />
+                <UKTextField color={"outlined"} label={"Password"} getValue={setPassword} />
+                <div class={styles.loginButtons}>
+                    <UKButton onClick={() => 0} disabled={username() === ""} color={"standard"}>
                         Forgot password?
                     </UKButton>
-                    <UKButton onClick={() => 0} color={"filled"}>
+                    <UKButton disabled={username() === "" || password() === ""} onClick={() => {
+                        console.log("Requesting login for", username(), password())
+                    }} color={"filled"}>
                         Login
                     </UKButton>
-                </UKButtonGroup>
-                <UKText role={"body"} size={"m"}>
-                    Don't have an account?
-                </UKText>
-                <UKButton onClick={() => 0} color={"tonal"}>
-                    Signup
-                </UKButton>
+                </div>
+                <UKDivider direction={DividerDirection.horizontal} />
+                <div class={styles.signupSegment}>
+                    <UKText role={"body"} size={"m"}>
+                        Don't have an account?
+                    </UKText>
+                    <UKButton onClick={() => 0} color={"tonal"}>
+                        Signup
+                    </UKButton>
+                </div>
             </UKCard>
+            <UKCard color={"outlined"} class={styles.copyrightContainer}>
+                <UKText role={"title"} size={"m"} emphasized={true}>
+                    Tricolor Workspaces Pre-Alpha
+                </UKText>
+                <UKText role={"body"} size={"s"} emphasized={true}>
+                    © Copyright Tricolor 2025
+                </UKText>
+            </UKCard>
+        </div>
     );
 };
 
