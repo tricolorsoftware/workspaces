76be9ea82397b5db6d36e6455696b90109e4fca1
ewsgit <ewsgit@ewsgit.uk>
2025-10-05 23:31:29 +0000

    add UKText, begin UKTextField, fix baseline missing or incorrectly named values

--- packages/web-frontend/src/pages/userSelect/UserSelect.tsx
+++ packages/web-frontend/src/pages/userSelect/UserSelect.tsx
@@ -1,17 +1,22 @@
 import type { Component } from "solid-js";
-import { page } from "./UserSelect.css.ts";
 import UKCard from "@tricolor/uikit-solid/src/components/card/UKCard.tsx";
 import UKButton from "@tricolor/uikit-solid/src/components/button/UKButton.tsx";
 import UKButtonGroup from "@tricolor/uikit-solid/src/components/buttonGroup/UKButtonGroup.tsx";
 import UKDivider from "@tricolor/uikit-solid/src/components/divider/UKDivider.tsx";
 import { DividerDirection } from "@tricolor/uikit-solid/src/components/divider/lib/direction.ts";
+import UKText from "@tricolor/uikit-solid/src/components/text/UKText.tsx";
+import styles from "./UserSelect.module.scss"
+import UKTextField from "@tricolor/uikit-solid/src/components/textField/UKTextField.tsx";
 
 const UserSelectPage: Component = () => {
     return (
-        <div class={page}>
-            <UKCard color={"filled"}>
-                <p class={"sampletest"}>Sign in</p>
+            <UKCard color={"filled"} class={styles.root}>
+                <UKText role={"title"} size={"l"}>
+                    Sign In
+                </UKText>
                 <UKDivider direction={DividerDirection.horizontal} />
+                <UKTextField color={"outlined"} label={"Username"}/>
+                <UKTextField color={"outlined"} label={"Password"}/>
                 <UKButtonGroup size={"s"}>
                     <UKButton onClick={() => 0} color={"standard"}>
                         Forgot password?
@@ -20,35 +25,13 @@ const UserSelectPage: Component = () => {
                         Login
                     </UKButton>
                 </UKButtonGroup>
-                <p>Don't have an account?</p>
+                <UKText role={"body"} size={"m"}>
+                    Don't have an account?
+                </UKText>
                 <UKButton onClick={() => 0} color={"tonal"}>
                     Signup
                 </UKButton>
             </UKCard>
-            <UKCard color={"elevated"}>
-                <UKDivider direction={DividerDirection.horizontal} />
-                <UKButtonGroup size={"s"}>
-                    <UKButton onClick={() => 0} color={"tonal"}>
-                        Signup
-                    </UKButton>
-                    <UKButton onClick={() => 0} color={"filled"}>
-                        Login
-                    </UKButton>
-                </UKButtonGroup>
-            </UKCard>
-            <UKCard color={"outlined"}>
-                <UKDivider direction={DividerDirection.horizontal} />
-                <UKButtonGroup size={"s"}>
-                    <UKButton onClick={() => 0} color={"tonal"}>
-                        Signup
-                    </UKButton>
-                    <UKButton onClick={() => 0} color={"filled"}>
-                        Login
-                    </UKButton>
-                </UKButtonGroup>
-            </UKCard>
-            User Select Page
-        </div>
     );
 };
 
