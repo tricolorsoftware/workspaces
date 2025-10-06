868063999ef95dc2beeb21f9f4f6869b53ce4d05
ewsgit <ewsgit@ewsgit.uk>
2025-10-07 00:07:16 +0100

    create database connections

--- packages/backend/src/subsystems/database.ts
+++ packages/backend/src/subsystems/database.ts
@@ -1,13 +1,34 @@
+import {SQL} from "bun";
 import type { Instance } from "../index.js";
 import SubSystem from "../subSystems.js";
 
 export default class DatabaseSubsystem extends SubSystem {
+    databaseConnections: {[connectionId: string]: SQL}
+
     constructor(instance: Instance) {
         super("database", instance);
+
+        this.databaseConnections = {}
+
         return this;
     }
 
-    createConnection(tableId: string) {
+    getConnection(connectionId: string) {
+        if (this.databaseConnections[ connectionId ])
+            return this.databaseConnections[ connectionId ]
 
+        return this.createConnection(connectionId)
+    }
+
+    createConnection(connectionId: string) {
+        let con = new SQL(connectionId, {
+            // NOTE: for now, in the future we should switch to postgres / a better alternative
+            adapter: "sqlite",
+            readonly: false
+        })
+
+        this.databaseConnections[ connectionId ] = con;
+
+        return con;
     }
 }
