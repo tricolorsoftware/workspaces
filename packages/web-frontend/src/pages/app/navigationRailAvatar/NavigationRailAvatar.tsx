6cd9a2b81468d8660c11598ee82092b94a47dd20
ewsgit <ewsgit@ewsgit.uk>
2025-10-21 16:49:04 +0000

    changes

--- packages/web-frontend/src/pages/app/navigationRailAvatar/NavigationRailAvatar.tsx
+++ packages/web-frontend/src/pages/app/navigationRailAvatar/NavigationRailAvatar.tsx
@@ -1,18 +1,25 @@
 import UKAvatar from "@tricolor/uikit-solid/src/components/avatar/UKAvatar.jsx";
-import type { Component } from "solid-js";
+import { createResource, type Component } from "solid-js";
 import styles from "./NavigationRailAvatar.module.scss";
 import UKText from "@tricolor/uikit-solid/src/components/text/UKText.jsx";
+import trpc from "../../../lib/trpc";
 
 const NavigationRailAvatar: Component<{ expanded: boolean }> = (props) => {
+    const [user] = createResource(() => trpc.app.navigation.user.name.query());
+
+    if (user()) {
+        return null;
+    }
+
     return (
         <div class={styles.root} data-expanded={props.expanded}>
             <UKAvatar class={styles.avatar} avatar={"/assets/placeholder/avatar.png"} size="s" username="[PUT SIDEBAR HERE]" />
             <div class={styles.nameContainer}>
                 <UKText size="m" role="title" class={styles.displayName}>
-                    Display Name
+                    {`${user.forename} ${user.surname}`}
                 </UKText>
                 <UKText size="m" role="label" class={styles.username}>
-                    {"@"}Username
+                    {`@${user.username}`}
                 </UKText>
             </div>
         </div>
