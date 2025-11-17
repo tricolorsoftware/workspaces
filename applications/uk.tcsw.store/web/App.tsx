78c8a10ba34d4b101a5fc150e54e86a7337d7100
ewsgit <ewsgit@ewsgit.uk>
2025-11-17 12:07:59 +0000

    remake appRouter

--- applications/uk.tcsw.store/web/App.tsx
+++ applications/uk.tcsw.store/web/App.tsx
@@ -1,5 +1,5 @@
 import { type Component } from "solid-js";
-import { Route } from "@solidjs/router";
+import { Route, Router } from "@solidjs/router";
 import RootPage from "./pages/root/page";
 
 const App: Component = () => {
