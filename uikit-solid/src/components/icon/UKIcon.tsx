708968693cbc4d75d8ddf3bedad930f64a9a3bd5
ewsgit <ewsgit@ewsgit.uk>
2025-11-22 16:19:43 +0000

    UIKit: clickable UKIcon

--- uikit-solid/src/components/icon/UKIcon.tsx
+++ uikit-solid/src/components/icon/UKIcon.tsx
@@ -3,8 +3,8 @@ import "./icon.css";
 import styles from "./UKIcon.module.scss"
 import clsx from "clsx";
 
-const UKIcon: Component<{ children: string; class?: string }> = (props) => {
-    return <span class={clsx(styles.root, props.class)}>{props.children}</span>;
+const UKIcon: Component<{children: string; class?: string; onClick?: () => void}> = (props) => {
+    return <span onClick={props.onClick} data-clickable={!!props.onClick} class={clsx(styles.root, props.class)}>{props.children}</span>;
 };
 
 export default UKIcon;
